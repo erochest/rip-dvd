8629da73081f2d3bac1ed308a8537e79bd284311
Eric Rochester <erochest@virginia.edu>
2014-11-07 05:20:33 -0500

    Command-line parsing.

--- Main.hs
+++ Main.hs
@@ -4,5 +4,29 @@
 module Main where
 
 
+import           Options.Applicative
+import           Options.Applicative.Types
+import qualified Data.Text as T
+
+
+newtype BaseName = BaseName { unBaseName :: T.Text }
+                   deriving (Show, Eq)
+
+
 main :: IO ()
-main = undefined
+main = print =<< execParser opts
+
+
+opts' :: Parser BaseName
+opts' =   BaseName
+      <$> argument readText (  metavar "BASENAME"
+                            <> help "Rips to dvds/[BASENAME].iso.")
+
+opts :: ParserInfo BaseName
+opts = info (helper <*> opts')
+            (  fullDesc
+            <> progDesc "Rips a DVD to an ISO."
+            <> header "rip-dvd - rips a DVD to an ISO.")
+
+readText :: ReadM T.Text
+readText = fmap T.pack readerAsk
